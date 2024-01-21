351a2d61dd525a92381b34feca22566285d2d54f
myssssyq <nabikussain@gmail.com>
2024-01-22 04:02:28 +0600

    Events and event pages added
    Signed-off-by: myssssyq <nabikussain@gmail.com>

---
+++ src/data/events.js
@@ -0,0 +1,22 @@
+const events = [
+    {
+      id: 1,
+      img: "/img/events/1.png",
+      date: "February 17, 2023",
+      title: "Blue Man Group",
+      delayAnimation: "100",
+      details: `Blue Man Group is comedy, theater, rock concert and dance party all rolled into one.`,
+      info: "This wildly popular phenomenon delivers an outrageous, multi-sensory experience you’ll never forget. Don’t miss the show that has captivated 35 million people worldwide. Blue Man Group—enemy of monotony, remedy for boredom, promoter of overjoy and elation."
+    },
+    {
+      id: 2,
+      img: "/img/events/2.png",
+      date: "January 19, 2023",
+      title: "Edge of Aquarius",
+      delayAnimation: "100",
+      details: `Alexandria Nunweiler with collaborative dance company The Click presents a new dance/theater work “Edge of Aquarius.”`,
+      info: "The performance takes inspiration from the surprisingly strange phenomenon of birthdays – the rituals surrounding them, the odd bonds people have with others who share theirs, the increasing malaise about them as people age, and the rites of passage and physical milestones they represent. Several years in the back of her mind as an idea, Nunweiler was compelled to create the work when iconic actress Betty White died just shy of her 100th birthday. (She and White shared a January 17 birthday.) Interwoven with personal stories from performers, fun and irreverent props, and fascinating birthday lore, this piece takes a microscope to aging and the way we celebrate and commemorate milestones. Cake is involved."
+    }
+  ]
+  
+  export default events;
