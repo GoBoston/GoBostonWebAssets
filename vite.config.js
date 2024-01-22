f4666fea3e689e5379b23e9066d1cddb1c69b244
daniyardake <daniyardake@gmail.com>
2024-01-22 14:06:44 -0500

    base fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GoBostonWebAssets/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
